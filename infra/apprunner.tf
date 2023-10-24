d4bd6bef3120313c286eb5c9234f393ab47938ac
bjarte <22171847+freebattie@users.noreply.github.com>
2023-10-24 16:56:03 +0200

    testing

--- infra/apprunner.tf
+++ infra/apprunner.tf
@@ -3,7 +3,7 @@ resource "aws_apprunner_service" "service" {
   source_configuration {
 
     authentication_configuration {
-      access_role_arn = "arn:aws:iam::244530008913:role/service-role/AppRunnerECRAccessRole"
+      access_role_arn = "arn:aws:iam::244530008913:role/bjne_test"
     }
 
     image_repository {
